d0d36c06da7d0743cce06e3271867b0671ee883c
naoto hamada <hamada@vis-its.com>
2020-04-01 00:32:01 +0900

    Add test code

--- index.js
+++ index.js
@@ -10,17 +10,44 @@ const config = {
 
 const client = new line.Client(config);
 
+const calendar = [
+  {
+    date: "20200401",
+    place: "阪神",
+    name: "大阪杯",
+    conditions: "4歳以上",
+    course: "芝2,000メートル"
+  },
+  {
+    date: "20200406",
+    place: "阪神",
+    name: "大阪杯",
+    conditions: "4歳以上",
+    course: "芝2,000メートル"
+  }
+];
+
 exports.sendLine = (pubSubEvent, context) => {
+  const today = dayjs().format("YYYYMMDD");
+
+  const keiba = calendar.find(cal => {
+    return cal.date === today;
+  });
+
+  if (!keiba) return;
 
   const message = {
-    type: 'text',
-    text: 'hogehoge'
+    type: "text",
+    text: `今日は${keiba.place}競馬場で${keiba.name}が開催されます。\n${keiba.conditions}\n${keiba.course}`
   };
 
-  client.pushMessage(process.env.group_id, message).then(() => {
-    console.log('push message done');
-  }).catch((err) => {
-    console.log('push message error');
-    console.log(err);
-  });
+  client
+    .pushMessage(process.env.group_id, message)
+    .then(() => {
+      console.log("push message done");
+    })
+    .catch(err => {
+      console.log("push message error");
+      console.log(err);
+    });
 };
