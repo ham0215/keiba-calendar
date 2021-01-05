8148acb56b9f5f88394e09c06bfb48fe5735af0a
naoto hamada <hamada@vis-its.com>
2021-01-05 22:39:49 +0900

    Update 2021

--- keiba-calendar.js
+++ keiba-calendar.js
@@ -1,172 +1,172 @@
 // 元ネタ
-// http://www.jra.go.jp/datafile/seiseki/replay/2020/g1.html
+https://www.jra.go.jp/datafile/seiseki/replay/2021/g1.html
 module.exports.keibaCalendar = [
   {
-    date: "20200223",
+    date: "20210221",
     place: "東京",
     name: "フェブラリーS",
     conditions: "4歳以上",
     course: "ダ1,600メートル"
   },
   {
-    date: "20200329",
+    date: "20210328",
     place: "中京",
     name: "高松宮記念",
     conditions: "4歳以上",
     course: "芝1,200メートル"
   },
   {
-    date: "20200405",
+    date: "20210404",
     place: "阪神",
     name: "大阪杯",
     conditions: "4歳以上",
     course: "芝2,000メートル"
   },
   {
-    date: "20200412",
+    date: "20210411",
     place: "阪神",
     name: "桜花賞",
     conditions: "3歳牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20200419",
+    date: "20210418",
     place: "中山",
     name: "皐月賞",
     conditions: "3歳牡・牝",
     course: "芝2,000メートル"
   },
   {
-    date: "20200503",
+    date: "20210502",
     place: "京都",
     name: "天皇賞（春）",
     conditions: "4歳以上",
     course: "芝3,200メートル"
   },
   {
-    date: "20200510",
+    date: "20210509",
     place: "東京",
     name: "NHKマイルC",
     conditions: "3歳牡・牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20200517",
+    date: "20210516",
     place: "東京",
     name: "ヴィクトリアマイル",
     conditions: "4歳以上牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20200524",
+    date: "20210523",
     place: "東京",
     name: "オークス",
     conditions: "3歳牝",
     course: "芝2,400メートル"
   },
   {
-    date: "20200531",
+    date: "20210530",
     place: "東京",
     name: "日本ダービー",
     conditions: "3歳牡・牝",
     course: "芝2,400メートル"
   },
   {
-    date: "20200607",
+    date: "20210606",
     place: "東京",
     name: "安田記念",
     conditions: "3歳以上",
     course: "芝1,600メートル"
   },
   {
-    date: "20200628",
+    date: "20210627",
     place: "阪神",
     name: "宝塚記念",
     conditions: "3歳以上",
     course: "芝2,200メートル"
   },
   {
-    date: "20201004",
+    date: "20211003",
     place: "中山",
     name: "スプリンターズS",
     conditions: "3歳以上",
     course: "芝1,200メートル"
   },
   {
-    date: "20201018",
+    date: "2021107",
     place: "京都",
     name: "秋華賞",
     conditions: "3歳牝",
     course: "芝2,000メートル"
   },
   {
-    date: "20201025",
+    date: "20211024",
     place: "京都",
     name: "菊花賞",
     conditions: "3歳牡・牝",
     course: "芝3,000メートル"
   },
   {
-    date: "20201101",
+    date: "20211031",
     place: "東京",
     name: "天皇賞（秋）",
     conditions: "3歳以上",
     course: "芝2,000メートル"
   },
   {
-    date: "20201115",
+    date: "20211114",
     place: "阪神",
     name: "エリザベス女王杯",
     conditions: "3歳以上牝",
     course: "芝2,200メートル"
   },
   {
-    date: "20201122",
+    date: "20211121",
     place: "阪神",
     name: "マイルチャンピオンシップ",
     conditions: "3歳以上",
     course: "芝1,600メートル"
   },
   {
-    date: "20201129",
+    date: "20211128",
     place: "東京",
     name: "ジャパンC",
     conditions: "3歳以上",
     course: "芝2,400メートル"
   },
   {
-    date: "20201206",
+    date: "20211205",
     place: "中京",
     name: "チャンピオンズC",
     conditions: "3歳以上",
     course: "ダ1,800メートル"
   },
   {
-    date: "20201213",
+    date: "20211212",
     place: "阪神",
     name: "阪神ジュベナイルF",
     conditions: "2歳牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20201220",
+    date: "20211219",
     place: "阪神",
     name: "朝日杯フューチュリティS",
     conditions: "2歳牡・牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20201226",
-    place: "中山",
-    name: "ホープフルS",
-    conditions: "2歳牡・牝",
-    course: "芝2,000メートル"
-  },
-  {
-    date: "20201227",
+    date: "20211226",
     place: "中山",
     name: "有馬記念",
     conditions: "3歳以上",
     course: "芝2,500メートル"
+  },
+  {
+    date: "20211228",
+    place: "中山",
+    name: "ホープフルS",
+    conditions: "2歳牡・牝",
+    course: "芝2,000メートル"
   }
 ];
