9eba6d76e07bc301d31e914eee8828643912ac47
Naoto Hamada <naoto.hamada@findy.co.jp>
2024-01-21 19:21:06 +0900

    Update calendar

--- keiba-calendar.js
+++ keiba-calendar.js
@@ -1,169 +1,170 @@
 // 元ネタ
-// https://www.jra.go.jp/datafile/seiseki/replay/2022/g1.html
+// https://www.jra.go.jp/datafile/seiseki/replay/2024/g1.html
+// 今年はBing Chatで生成
 module.exports.keibaCalendar = [
   {
-    date: "20230219",
+    date: "20240218",
     place: "東京",
     name: "フェブラリーS",
     conditions: "4歳以上",
     course: "ダ1,600メートル"
   },
   {
-    date: "20230326",
+    date: "20240324",
     place: "中京",
     name: "高松宮記念",
     conditions: "4歳以上",
     course: "芝1,200メートル"
   },
   {
-    date: "20230402",
+    date: "20240331",
     place: "阪神",
     name: "大阪杯",
     conditions: "4歳以上",
     course: "芝2,000メートル"
   },
   {
-    date: "20230409",
+    date: "20240407",
     place: "阪神",
     name: "桜花賞",
     conditions: "3歳牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20230416",
+    date: "20240414",
     place: "中山",
     name: "皐月賞",
     conditions: "3歳牡・牝",
     course: "芝2,000メートル"
   },
   {
-    date: "20230430",
+    date: "20240428",
     place: "京都",
     name: "天皇賞（春）",
     conditions: "4歳以上",
     course: "芝3,200メートル"
   },
   {
-    date: "20230507",
+    date: "20240505",
     place: "東京",
     name: "NHKマイルC",
     conditions: "3歳牡・牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20230514",
+    date: "20240512",
     place: "東京",
     name: "ヴィクトリアマイル",
     conditions: "4歳以上牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20230521",
+    date: "20240519",
     place: "東京",
     name: "オークス",
     conditions: "3歳牝",
     course: "芝2,400メートル"
   },
   {
-    date: "20230528",
+    date: "20240526",
     place: "東京",
     name: "日本ダービー",
     conditions: "3歳牡・牝",
     course: "芝2,400メートル"
   },
   {
-    date: "20230604",
+    date: "20240602",
     place: "東京",
     name: "安田記念",
     conditions: "3歳以上",
     course: "芝1,600メートル"
   },
   {
-    date: "20230625",
+    date: "20240623",
     place: "阪神",
     name: "宝塚記念",
     conditions: "3歳以上",
     course: "芝2,200メートル"
   },
   {
-    date: "20231001",
+    date: "20240929",
     place: "中山",
     name: "スプリンターズS",
     conditions: "3歳以上",
     course: "芝1,200メートル"
   },
   {
-    date: "20231015",
+    date: "20241013",
     place: "京都",
     name: "秋華賞",
     conditions: "3歳牝",
     course: "芝2,000メートル"
   },
   {
-    date: "20231022",
+    date: "20241020",
     place: "京都",
     name: "菊花賞",
     conditions: "3歳牡・牝",
     course: "芝3,000メートル"
   },
   {
-    date: "20231029",
+    date: "20241027",
     place: "東京",
     name: "天皇賞（秋）",
     conditions: "3歳以上",
     course: "芝2,000メートル"
   },
   {
-    date: "20231112",
+    date: "20241110",
     place: "京都",
     name: "エリザベス女王杯",
     conditions: "3歳以上牝",
     course: "芝2,200メートル"
   },
   {
-    date: "20231119",
+    date: "20241117",
     place: "京都",
     name: "マイルチャンピオンシップ",
     conditions: "3歳以上",
     course: "芝1,600メートル"
   },
   {
-    date: "20231126",
+    date: "20241124",
     place: "東京",
     name: "ジャパンC",
     conditions: "3歳以上",
     course: "芝2,400メートル"
   },
   {
-    date: "20231203",
+    date: "20241201",
     place: "中京",
     name: "チャンピオンズC",
     conditions: "3歳以上",
     course: "ダ1,800メートル"
   },
   {
-    date: "20231210",
+    date: "20241208",
     place: "阪神",
     name: "阪神ジュベナイルF",
     conditions: "2歳牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20231217",
+    date: "20241215",
     place: "阪神",
     name: "朝日杯フューチュリティS",
     conditions: "2歳牡・牝",
     course: "芝1,600メートル"
   },
   {
-    date: "20231224",
+    date: "20241222",
     place: "中山",
     name: "有馬記念",
     conditions: "3歳以上",
     course: "芝2,500メートル"
   },
   {
-    date: "20231228",
+    date: "20241228",
     place: "中山",
     name: "ホープフルS",
     conditions: "2歳牡・牝",
