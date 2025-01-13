907d393643b19301e907259230e456d605e61a5c
Naoto Hamada <naoto.hamada@findy.co.jp>
2025-01-13 13:24:27 +0900

    Update package

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ exports.sendLine = (pubSubEvent, context) => {
       type: "text",
       text: `今日は${keiba.place}競馬場で${keiba.name}が開催されます。\n${keiba.conditions}\n${keiba.course}`
     };
-  } else if (today === '20240121') {
+  } else if (today === '20240113') {
     message = {
       type: "text",
-      text: "遅くなりましたが、中川ちゃんおめでとうございます！"
+      text: "遅くなりましたが、今年もよろしくお願いします！"
     };
   } else if (process.env.CI) {
     message = {
