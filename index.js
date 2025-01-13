4ae715262c546ca800b6d30f88a1241a701c9cb6
Naoto Hamada <naoto.hamada@findy.co.jp>
2025-01-13 22:20:57 +0900

    Refactor cloud run function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const express = require('express');
 const line = require("@line/bot-sdk");
 const dayjs = require("dayjs");
 require("dayjs/locale/ja");
@@ -12,45 +13,56 @@ const config = {
 
 const client = new line.Client(config);
 
-exports.sendLine = (pubSubEvent, context) => {
-  const today = dayjs()
-    .add(9, "hour")
-    .format("YYYYMMDD");
-  console.log(dayjs().add(9, "hour"));
-  console.log(today);
-  const keiba = keibaCalendar.find(cal => {
-    return cal.date === today;
-  });
+const app = express();
+app.use(express.json());
 
-  let message;
-  if (keiba) {
-    console.log(keiba);
-    message = {
-      type: "text",
-      text: `今日は${keiba.place}競馬場で${keiba.name}が開催されます。\n${keiba.conditions}\n${keiba.course}`
-    };
-  } else if (today === '20240112') {
-    message = {
-      type: "text",
-      text: "遅くなりましたが、今年もよろしくお願いします！"
-    };
-  } else if (process.env.CI) {
-    message = {
-      type: "text",
-      text: "keiba-calendar is running on CI"
-    };
-  }
-
-  if (!message) return;
-
-  console.log(message);
-  client
-    .pushMessage(process.env.group_id, message)
-    .then(() => {
-      console.log("push message done");
-    })
-    .catch(err => {
-      console.log("push message error");
-      console.log(err);
+app.post('/', async (req, res) => {
+  try {
+    const today = dayjs()
+      .add(9, "hour")
+      .format("YYYYMMDD");
+    console.log(dayjs().add(9, "hour"));
+    console.log(today);
+    const keiba = keibaCalendar.find(cal => {
+      return cal.date === today;
     });
-};
+
+    let lineMessage;
+    if (keiba) {
+      console.log("Found keiba event:", keiba);
+      lineMessage = {
+        type: 'text',
+        text: `今日は${keiba.place}競馬場で${keiba.name}が開催されます。\n${keiba.conditions}\n${keiba.course}`,
+      };
+    } else if (dateToUse === '20240112') {
+      lineMessage = {
+        type: 'text',
+        text: '遅くなりましたが、今年もよろしくお願いします！',
+      };
+    } else if (process.env.CI) {
+      lineMessage = {
+        type: 'text',
+        text: 'keiba-calendar is running on CI',
+      };
+    }
+
+    if (!lineMessage) {
+      return res.status(204).send('No Content');
+    }
+
+    // LINE メッセージの送信
+    console.log("Sending message:", lineMessage);
+    await client.pushMessage(process.env.group_id, lineMessage);
+    console.log('Push message sent successfully.');
+    res.status(200).send('Message sent successfully.');
+
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send(`Internal Server Error: ${err.message}`);
+  }
+});
+
+const PORT = parseInt(parseInt(process.env.PORT)) || 8080;
+app.listen(PORT, () =>
+  console.log(`nodejs-pubsub-tutorial listening on port ${PORT}`)
+);
